c973fce272b6b03d3a10f559d906978cd2e3a1bf
Alexander Khodko <alexanderkhodko@macbook-air-alexander.local>
2015-11-23 09:36:40 +0300

    Initial Commit

---
+++ bitkomprealpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitkomprealpha
+//
+//  Created by Alexander Khodko on 23.11.15.
+//  Copyright © 2015 Alexander Khodko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
